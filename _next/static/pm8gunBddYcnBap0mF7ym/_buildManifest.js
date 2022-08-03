7d011d4bf6b1ccb03d1953181c8566f121952dc7
johnweland <john.weland@gmail.com>
2022-08-03 17:51:49 +0000

    Deploying to gh-pages from @ johnweland/for-jackson@bfea393533c2dacc797d2d74556e616e31105276 🚀

--- _next/static/pm8gunBddYcnBap0mF7ym/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/551-461a284dd2ba9f49.js","static/chunks/pages/index-a508d63a440f7cc2.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
