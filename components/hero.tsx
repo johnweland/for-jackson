51d4f1b900801161a5c131fa018392053cee96cd
John Weland <john.weland@gmail.com>
2022-08-03 19:38:38 -0500

    adding to values

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,20 +1,19 @@
-import { Fragment } from 'react'
-import Image from 'next/image'
-import { Popover, Transition } from '@headlessui/react'
-import { MenuIcon, XIcon } from '@heroicons/react/outline'
+import { Fragment } from "react";
+import Image from "next/image";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const navigation = [
-    { name: 'Home', href: '#top' },
-    { name: 'About', href: '#about' },
-    { name: 'Core Values', href: '#values' },
-    { name: 'In The Community', href: '#timeline' },
-]
-
+  { name: "Home", href: "#top" },
+  { name: "About", href: "#about" },
+  { name: "Core Values", href: "#values" },
+  { name: "In The Community", href: "#timeline" },
+];
 
 const toBase64 = (str: string) =>
-    typeof window === 'undefined'
-        ? Buffer.from(str).toString('base64')
-        : window.btoa(str)
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
 
 const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
@@ -28,134 +27,142 @@ const shimmer = (w: number, h: number) => `
   <rect width="${w}" height="${h}" fill="#333" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
+</svg>`;
 
 const Hero = () => {
-    return (
-        <div id="top" className="relative bg-white overflow-hidden">
-            <div className="max-w-7xl mx-auto">
-                <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-                    <svg
-                        className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
-                        fill="currentColor"
-                        viewBox="0 0 100 100"
-                        preserveAspectRatio="none"
-                        aria-hidden="true"
-                    >
-                        <polygon points="50,0 100,0 50,100 0,100" />
-                    </svg>
+  return (
+    <div id="top" className="relative bg-white overflow-hidden">
+      <div className="max-w-7xl mx-auto">
+        <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
+          <svg
+            className="hidden lg:block absolute right-0 inset-y-0 h-full w-48 text-white transform translate-x-1/2"
+            fill="currentColor"
+            viewBox="0 0 100 100"
+            preserveAspectRatio="none"
+            aria-hidden="true"
+          >
+            <polygon points="50,0 100,0 50,100 0,100" />
+          </svg>
 
-                    <Popover>
-                        <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
-                            <nav className="relative flex items-center justify-between sm:h-10 lg:justify-start" aria-label="Global">
-                                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
-                                    <div className="flex items-center justify-between w-full md:w-auto">
-                                        <a href="#">
-                                            <span className="sr-only">John Weland for Jackson</span>
-                                            {/* <img
+          <Popover>
+            <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
+              <nav
+                className="relative flex items-center justify-between sm:h-10 lg:justify-start"
+                aria-label="Global"
+              >
+                <div className="flex items-center flex-grow flex-shrink-0 lg:flex-grow-0">
+                  <div className="flex items-center justify-between w-full md:w-auto">
+                    <a href="#">
+                      <span className="sr-only">John Weland for Jackson</span>
+                      {/* <img
                                                 alt="Workflow"
                                                 className="h-8 w-auto sm:h-10"
                                                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                                             /> */}
-                                        </a>
-                                        <div className="-mr-2 flex items-center md:hidden">
-                                            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
-                                                <span className="sr-only">Open main menu</span>
-                                                <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                                            </Popover.Button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
-                                    {navigation.map((item) => (
-                                        <a key={item.name} href={item.href} className="font-medium text-gray-500 hover:text-gray-900">
-                                            {item.name}
-                                        </a>
-                                    ))}
-                                </div>
-                            </nav>
-                        </div>
+                    </a>
+                    <div className="-mr-2 flex items-center md:hidden">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
+                        <span className="sr-only">Open main menu</span>
+                        <MenuIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                </div>
+                <div className="hidden md:block md:ml-10 md:pr-4 md:space-x-8">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="font-medium text-gray-500 hover:text-gray-900"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+              </nav>
+            </div>
 
-                        <Transition
-                            as={Fragment}
-                            enter="duration-150 ease-out"
-                            enterFrom="opacity-0 scale-95"
-                            enterTo="opacity-100 scale-100"
-                            leave="duration-100 ease-in"
-                            leaveFrom="opacity-100 scale-100"
-                            leaveTo="opacity-0 scale-95"
-                        >
-                            <Popover.Panel
-                                focus
-                                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
-                            >
-                                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-                                    <div className="px-5 pt-4 flex items-center justify-between">
-                                        <div>
-                                            {/* <img
+            <Transition
+              as={Fragment}
+              enter="duration-150 ease-out"
+              enterFrom="opacity-0 scale-95"
+              enterTo="opacity-100 scale-100"
+              leave="duration-100 ease-in"
+              leaveFrom="opacity-100 scale-100"
+              leaveTo="opacity-0 scale-95"
+            >
+              <Popover.Panel
+                focus
+                className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top-right md:hidden"
+              >
+                <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
+                  <div className="px-5 pt-4 flex items-center justify-between">
+                    <div>
+                      {/* <img
                                                 className="h-8 w-auto"
                                                 src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                                                 alt=""
                                             /> */}
-                                        </div>
-                                        <div className="-mr-2">
-                                            <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
-                                                <span className="sr-only">Close main menu</span>
-                                                <XIcon className="h-6 w-6" aria-hidden="true" />
-                                            </Popover.Button>
-                                        </div>
-                                    </div>
-                                    <div className="px-2 pt-2 pb-3 space-y-1">
-                                        {navigation.map((item) => (
-                                            <a
-                                                key={item.name}
-                                                href={item.href}
-                                                className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
-                                            >
-                                                {item.name}
-                                            </a>
-                                        ))}
-                                    </div>
-                                </div>
-                            </Popover.Panel>
-                        </Transition>
-                    </Popover>
-
-                    <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
-                        <div className="sm:text-center lg:text-left">
-                            <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-                                <span className="block xl:inline">John Weland</span>{' '}
-                                <span className="block text-emerald-600 xl:inline">For Jackson City Council</span>
-                            </h1>
-                            <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
-                                fugiat veniam occaecat fugiat aliqua.
-                            </p>
-                            <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
-
-                            </div>
-                        </div>
                     </div>
+                    <div className="-mr-2">
+                      <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-emerald-500">
+                        <span className="sr-only">Close main menu</span>
+                        <XIcon className="h-6 w-6" aria-hidden="true" />
+                      </Popover.Button>
+                    </div>
+                  </div>
+                  <div className="px-2 pt-2 pb-3 space-y-1">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-gray-900 hover:bg-gray-50"
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
                 </div>
-            </div>
-            <div className="relative lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
-                <Image
-                    className="h-[30rem] w-full object-cover object-[0, 100px, 0, 0] sm:h-[35rem] md:h-[45rem] lg:w-full lg:h-[55rem]"
-                    src="https://scontent-msp1-1.xx.fbcdn.net/v/t1.6435-9/120314998_10164150856275627_7446608234314951746_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=0debeb&_nc_ohc=UU7An8eKPYwAX9d6tmK&_nc_ht=scontent-msp1-1.xx&oh=00_AT8X9QrZ7iQQV4r2_2k3kcNktGeJAwu9PIZ7NIHd44ye2Q&oe=62EE14CC"
-                    alt=""
-                    priority
-                    quality={65}
-                    loading="eager"
-                    layout="responsive"
-                    objectFit='cover'
-                    width={880}
-                    height={880}
-                    placeholder="blur"
-                    blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(700, 475))}`}
-                />
-            </div>
-        </div>
-    )
-}
+              </Popover.Panel>
+            </Transition>
+          </Popover>
 
-export default Hero
+          <div className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+            <div className="sm:text-center lg:text-left">
+              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+                <span className="block xl:inline">John Weland</span>{" "}
+                <span className="block text-emerald-600 xl:inline">
+                  For Jackson City Council
+                </span>
+              </h1>
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                The right choice in moving Jackson forward.
+              </p>
+              <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="relative lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2">
+        <Image
+          className="h-[30rem] w-full object-cover object-[0, 100px, 0, 0] sm:h-[35rem] md:h-[45rem] lg:w-full lg:h-[55rem]"
+          src="https://scontent-msp1-1.xx.fbcdn.net/v/t1.6435-9/120314998_10164150856275627_7446608234314951746_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=0debeb&_nc_ohc=UU7An8eKPYwAX9d6tmK&_nc_ht=scontent-msp1-1.xx&oh=00_AT8X9QrZ7iQQV4r2_2k3kcNktGeJAwu9PIZ7NIHd44ye2Q&oe=62EE14CC"
+          alt=""
+          priority
+          quality={65}
+          loading="eager"
+          layout="responsive"
+          objectFit="cover"
+          width={880}
+          height={880}
+          placeholder="blur"
+          blurDataURL={`data:image/svg+xml;base64,${toBase64(
+            shimmer(700, 475)
+          )}`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
