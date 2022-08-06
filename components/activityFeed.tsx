7d3294c35dbab52cd146aef2be03ff9f88de7f8f
John Weland <john.weland@gmail.com>
2022-08-05 22:31:23 -0500

    update values

--- components/activityFeed.tsx
+++ components/activityFeed.tsx
@@ -36,14 +36,14 @@ const activity = [
     id: 2,
     type: "community",
     person: { name: "Jackson in Action", href: "#" },
-    comment: `Joined the Jackson in Action community, a townhall format discussion put together by community leader Jeff Gay. \n Jackson in Action was held semi-regularly to foster the discussion of issues and ideas about the Jackson area in hopes to facilitate growth in our community.`,
+    comment: `Joined the Jackson in Action community, a town hall format discussion group put together by community leader Jeff Gay. \n Jackson in Action was held semi-regularly to foster the discussion of issues and ideas about the Jackson area in hopes to facilitate growth in our community.`,
     date: "2017",
   },
   {
     id: 3,
     type: "community",
     person: { name: "Animal Committee", href: "#" },
-    comment: `Mayor Wayne Walters decried a need for an Animal Committee to identify a solution to address community concern regarding the "Stray Cat" situation in the downtown area. \n The committee was lead by aldermen Jeff Gay, as a committee member I helped to found the animal rescue JCAPS as a  legally registered non-profit.`,
+    comment: `Mayor Wayne Walters tasked Alderman Jeff Gay with the formation of an Animal Committee to address and resolve the community concerns regarding the stray cat situation in the downtown area. \n The committee was led by Alderman Jeff Gay, and as a committee member I helped to found the Jackson County Animal Protection Society (JCAPS) as a legally registered 501(c)3 organization.`,
     date: "January 2020",
   },
   {
@@ -55,15 +55,14 @@ const activity = [
     },
     imageURL:
       "https://scontent-msp1-1.xx.fbcdn.net/v/t39.30808-6/240651267_116271594097649_8581966275091961355_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=8XM6DLAgo4EAX_RcOlh&_nc_ht=scontent-msp1-1.xx&oh=00_AT-yD6bJJNulsr8TtzqI9shy_8FzC5bNf7I2jWobmJsUwQ&oe=62EFF8F9",
-    comment: `I serve as the appointed President of JCAPS to oversee the day-to-day operations for the Board of Directors \n Since its inception in 2016, JCAPS has taken in, provided medical care for and rehomed 40 animals in Jackson County and counting.`,
+    comment: `I currently serve as the appointed President of JCAPS to oversee the day-to-day operations for the Board of Directors. \n Since its inception in 2020, JCAPS has taken in, provided medical care for, and rehomed 40 animals (and counting) in Jackson County.`,
     date: "2020 - Current",
   },
   {
     id: 5,
     type: "community",
     person: { name: "Community Gardens", href: "#" },
-    comment: `I have helped to establish the first community garden at Zimmerli Park. As a volunteer for the "Green Thumbs" Garden Comittee, I help with the upkeep of the community gardens and to plan the future of the community gardens. \n
-            The community gardens support social interactions, teach S.T.E.A.M, with hopes to provide exess fresh produce for community programs such as Sinor Dining and the Food Shelf.`,
+    comment: `I have helped to establish the first community garden at Zimmerli Park. The community garden support S.T.E.A.M education programs, with the hopes to provide the excess fresh produce for community programs such as Senior Dining and the Food Shelf. \n As a volunteer for the "Green Thumbs" Garden Comittee, I participate in the upkeep and expansion of the Zimmerli community garden and in the planning for future community gardens.`,
     date: "March 2021 - Current",
   },
   {
@@ -72,14 +71,14 @@ const activity = [
     person: { name: "A Pet's Tale", href: "#" },
     imageURL:
       "https://scontent-msp1-1.xx.fbcdn.net/v/t1.6435-9/209202562_122432050044822_5113489918730997331_n.png?_nc_cat=103&ccb=1-7&_nc_sid=e3f864&_nc_ohc=WVNwuoMVqScAX-V-swF&_nc_ht=scontent-msp1-1.xx&oh=00_AT9nfThLZIM-6QMlIEvTOTSaBOG-4jOtD2DTL0nqEjXL6A&oe=62F17F73",
-    comment: `Under the instruction of the Board of Directors, I have assited in starting the pet supply store A Pet\'s Tale. \n This provides a needed aminitee for the community and acts as an income source for JCAPS allowing the non-profit to continue to cover the spay/neuter and vaccinations for the animals.`,
+    comment: `With the oversight of the Board of Directors, A Pet's Tale (a pet supply store) was conceived in order to provide a necessary service for the community, and to provide financial resources for JCAPS, thus allowing the non-profit to continue to cover spay/neuter surgeries, vaccinations, and other needs for the animals that JCAPS takes in.`,
     date: "August 2021",
   },
   {
     id: 7,
     type: "speaker",
     person: { name: "Jackson in Action: Podcast", href: "#" },
-    comment: `I've began a podcast called "Jackson in Action"  to carry the torch of my mentor. The goal is to talk about the Jackson area and the community. \n The podcast will cover topics such as community issues and think-tank ideas in hopes to spark curiosity and interest and facilitate growth in the community.`,
+    comment: `I am in the process of creating a podcast called "Jackson in Action" (a personal passion project). I developed this idea with input from Jeff Gay as a partner and mentor on the project. Now, I want to bring those ideas and concepts to fruition. The goal is to talk about the Jackson area and the community. \n The podcast will cover topics such as community issues and think-tank ideas, with the hope of sparking discussion and action, and to help facilitate growth in the community.`,
     date: "July 2022",
   },
 ];
